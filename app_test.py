75aa5df7998b45fd975449b15cab83943d59be3a
shigemk2 <i.am.shige@gmail.com>
2019-12-20 20:50:55 +0900

    Bump Python version from 2.7 to 3.8

--- app_test.py
+++ app_test.py
@@ -9,7 +9,7 @@ class CitiesTestCase(unittest.TestCase):
     tester = app.test_client(self)
     response = tester.get('/cities.json', content_type='application/json')
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data, json.dumps({"cities" : ["Amsterdam", "Berlin", "New York", "San Francisco", "Tokyo"]}))
+    self.assertEqual(response.data, b'{"cities": ["Amsterdam", "Berlin", "New York", "San Francisco", "Tokyo"]}')
 
 if __name__ == '__main__':
     unittest.main()
